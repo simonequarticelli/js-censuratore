62a280a49c8903a652160c1c0b9d5cd34270efeb
simonequarticelli <quarti87@gmail.com>
2019-05-04 20:32:22 +0200

    inizio progetto js-censuratore

---
+++ main.js
@@ -0,0 +1,4 @@
+// - Il software riceve in input un lungo testo e una serie di parole da censurare.
+// - Restituisce il testo con xxx al posto delle parole censurate.
+// - Stampa un “badword index” calcolato come il numero di parole censurate
+//  su il numero di parole totali
