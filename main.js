99dd7a03235c7b5dfc5f77aeac9bd4edd444db59
simonequarticelli <quarti87@gmail.com>
2019-05-05 18:38:51 +0200

    aggiunto style e ottimizzazione

--- main.js
+++ main.js
@@ -5,37 +5,39 @@
 
 //creo input utente
 var testo = prompt('inserisci un testo');
-//console.log(testo);
-
-//uso funzione per convertire in "array"
-testo = testo.split(' ');
-//console.log(testo);
 
 //creo array di parole vietate
 var censura = ['blu', 'rosso', 'giallo', 'verde', 'arancione'];
-//console.log(censura);
 
-//creo array popolato dalle parole censurate inserite dall'utente
-var word_user = [];
+//creo funzione che censurare parole
+function censored_words(prompt_testo, array_parole_vietate){
+  //uso funzione per convertire in "array"
+  prompt_testo = prompt_testo.split(' ');
 
-//creo contatore di parole censurate inserite
-var contatore = 0;
+  //creo array popolato dalle parole censurate inserite dall'utente
+  var word_user = [];
 
+  //creo contatore di parole censurate inserite
+  var contatore = 0;
 
-var prova = false;
-var i = 0;
-while (i <= testo.length) {
-  if (censura.includes(testo[i])) {
-    prova = true;
-    word_user.push(testo[i]);
-    testo[i] = 'xxx';
-    contatore = contatore + 1;
+  //creo log per visualizzare il testo non censurato
+  console.log(prompt_testo.join(' '));
+
+  var find = false;
+  var i = 0;
+  while (i <= prompt_testo.length) {
+    if (array_parole_vietate.includes(prompt_testo[i])) {
+      find = true;
+      word_user.push(prompt_testo[i]);
+      prompt_testo[i] = 'xxx';
+      contatore = contatore + 1;
+    }
+    i++;
   }
-  i++;
+  document.getElementById('testo').innerHTML=(prompt_testo.join(' ') + "<br>");
+  console.log('hai inserito ' + contatore + ' parole vietate su ' + prompt_testo.length);
+  console.log(word_user);
 }
 
-alert(testo);
-alert('hai inserito ' + contatore + ' parole vietate su ' + testo.length);
-console.log('hai inserito ' + contatore + ' parole vietate su ' + testo.length);
-console.log(testo);
-console.log(word_user);
+//richiamo la funzione appena creata
+censored_words(testo, censura);
